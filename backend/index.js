e215b1a30e89a4315f19e21bd5c573b255a0f73f
ajeetsingh100 <ajeetsinghjb@gmail.com>
2026-03-06 11:17:25 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(express.json())
 app.use(express.urlencoded())
 app.use(cookieParser())
 app.use(cors({
-    origin:"https://codecatalyst-frontend.vercel.app/",
+    origin:"https://codecatalyst-frontend.vercel.app",
     credentials:true
 }))
 
